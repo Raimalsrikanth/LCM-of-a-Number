7d5bdd8e55e97986158282b32a6493e6dd5c79ee
Raimalsrikanth <96488129+Raimalsrikanth@users.noreply.github.com>
2021-12-30 20:47:58 +0530

    Created LCM.C
    This file prints LCM Of two numbers on the screen

---
+++ LCM.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<Conio.h>
+int main()
+{
+int num,num 1;
+Printf("Enter First Number");
+Scanf("%d",&num);
+Printf("Enter second Number");
+Scanf("&d",&num1);
+Printf("LCM=%d",1cm(num,num 1));
+}
+int 1 cm(int a,int b)
+{
+Static int m=0;
+m=m+b;
+if(m%a==0 && m%b==0)
+return m;
+return 1cm (a,b)
+}
